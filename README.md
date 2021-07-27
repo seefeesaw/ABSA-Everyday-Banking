bfd56762bdd6288a11d311f9c3426771d52efb33
seefeesaw <ashongwe@student.wethinkcode.co.za>
2021-07-27 23:08:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-x7pp39
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-x7pp39)
---
+++ index.js
@@ -0,0 +1,7 @@
+// run `node index.js` in the terminal
+
+var time = Date.now || function() {
+  return +new Date;
+};
+
+time();
